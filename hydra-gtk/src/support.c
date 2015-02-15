92f5d4f6b278aacf93e3072bc4fba7512a4a9503
Clayton Walker <clayton.m.walker@gmail.com>
2015-02-15 00:40:59 +0000

    Remove direct struct accessor in support.c

--- hydra-gtk/src/support.c
+++ hydra-gtk/src/support.c
@@ -24,7 +24,7 @@ GtkWidget *lookup_widget(GtkWidget * widget, const gchar * widget_name) {
     if (GTK_IS_MENU(widget))
       parent = gtk_menu_get_attach_widget(GTK_MENU(widget));
     else
-      parent = widget->parent;
+      parent = gtk_widget_get_parent(widget);
     if (!parent)
       parent = (GtkWidget *) g_object_get_data(G_OBJECT(widget), "GladeParentKey");
     if (parent == NULL)
