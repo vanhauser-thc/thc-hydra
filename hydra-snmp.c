ae324d6544e36251dedda0c5e20049d04eab776e
Bernard Spil <spil.oss@gmail.com>
2015-03-12 08:57:57 +0100

    Replace deprecated des_ and fix LibreSSL build
    uses deprecated des_ methods and types that were marked deprecated by
    OpenSSL 0.9.7 and will be removed in OpenSSL 1.1.0 . This patch replaces
    the des_ methods and types with their new DES_ counterparts. This
    enables building with LibreSSL as OpenSSL library.

--- hydra-snmp.c
+++ hydra-snmp.c
@@ -335,8 +335,8 @@ int start_snmp(int s, char *ip, int port, unsigned char options, char *miscptr, 
       // xor initVect with salt  
       for (i = 0; i < 8; i++)
         initVect[i] ^= privacy_params[i];
-      des_key_sched((C_Block *) key, symcbc);
-      des_ncbc_encrypt(snmpv3_get2 + 2, buf, sizeof(snmpv3_get2) - 2, symcbc, (C_Block *) (initVect), DES_ENCRYPT);
+      DES_key_sched((DES_cblock *) key, &symcbc);
+      DES_ncbc_encrypt(snmpv3_get2 + 2, buf, sizeof(snmpv3_get2) - 2, &symcbc, (DES_cblock *) (initVect), DES_ENCRYPT);
 
 #endif
 
