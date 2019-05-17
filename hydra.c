ab467e0a3b266cd7426f7c329d7a5f94b86ae9fb
van Hauser <vh@thc.org>
2019-05-17 08:50:01 +0200

    v9.0 release

--- hydra.c
+++ hydra.c
@@ -214,7 +214,7 @@ char *SERVICES =
 #define RESTOREFILE "./hydra.restore"
 
 #define PROGRAM   "Hydra"
-#define VERSION   "v8.9-dev"
+#define VERSION   "v9.0"
 #define AUTHOR    "van Hauser/THC"
 #define EMAIL     "<vh@thc.org>"
 #define RESOURCE  "https://github.com/vanhauser-thc/thc-hydra"
