26ab0f8c6ffffcef8ed1a90bf26f3627177f9a25
owein <yvain29@gmail.com>
2019-10-21 20:37:25 +0200

    !!! I did not test the escapes in the miscptr !!!

--- hydra.c
+++ hydra.c
@@ -504,6 +504,7 @@ void help(int32_t ext) {
 #ifdef HAVE_MATH_H
                     "  -x MIN:MAX:CHARSET  password bruteforce generation, type \"-x -h\" to get help\n"
                     "  -y        disable use of symbols in bruteforce, see above\n"
+                    "  -r        rainy mode for password generation (-x)\n"
 #endif
                "  -e nsr    try \"n\" null password, \"s\" login as pass "
                "and/or \"r\" reversed login\n"
