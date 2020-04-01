88637abe26e1b5cced945038c831a7cad654ea4a
van Hauser <vh@thc.org>
2020-04-01 12:26:28 +0200

    fix typo

--- hydra.c
+++ hydra.c
@@ -3353,7 +3353,7 @@ int main(int argc, char *argv[]) {
     // script kiddie patch
     if (hydra_options.server != NULL && (hydra_strcasestr(hydra_options.server, ".outlook.com") != NULL || hydra_strcasestr(hydra_options.server, ".hotmail.com") != NULL || hydra_strcasestr(hydra_options.server, ".yahoo.") != NULL || hydra_strcasestr(hydra_options.server, ".gmx.") != NULL || hydra_strcasestr(hydra_options.server, ".web.de") != NULL || hydra_strcasestr(hydra_options.server, ".gmail.") != NULL || hydra_strcasestr(hydra_options.server, "googlemail.") != NULL)) {
       fprintf(stderr, "[WARNING] Google Mail and others have bruteforce and "
-                      "hydra detection and sends false positives. You are not "
+                      "hydra detection and send false positives. You are not "
                       "doing anything illegal right?!\n");
       fprintf(stderr, "[WARNING] !read the above!\n");
       sleep(5);
