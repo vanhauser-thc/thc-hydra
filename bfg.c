1a1bcf38550054628f76314f06e0c0f00b323da5
owein <o20@localhost.localdomain>
2020-11-14 14:20:00 +0100

    uptodate with rainycrack

--- bfg.c
+++ bfg.c
@@ -202,6 +202,14 @@ uint64_t bf_get_pcount() {
   return foo;
 }
 
+
+int accu(int x) {
+    int a = 0, b;
+    for(b=1; b<x; ++b)
+        a+=b;
+    return a;
+}
+
 char *bf_next(_Bool rainy) {
   int32_t i, pos = bf_options.current - 1;
 
@@ -219,9 +227,10 @@ char *bf_next(_Bool rainy) {
     bf_options.ptr[0] = bf_options.crs[bf_options.state[0]];
     for(i=1; i<bf_options.current; ++i) {
 	  bf_options.ptr[i] = bf_options.crs[(bf_options.state[i] + bf_options.rain) % bf_options.crs_len];
-	  bf_options.rain /= bf_options.gcounter % (bf_options.crs_len / 2) + 1;
-      bf_options.gcounter+=2;
+	  bf_options.rain -= bf_options.rain / bf_options.crs_len;
+      bf_options.gcounter+=i;
     }
+    bf_options.gcounter -= accu(bf_options.current)-1;
   }
   else
     for(i=0; i<bf_options.current; ++i)
