1174cf490c6064e3860c7a58d95f50c7a989a134
Arnaud Rebillout <arnaudr@debian.org>
2025-11-21 11:06:01 +0700

    hydra-gtk: Reduce make_hydra.sh to the minimum
    Following the logic of the last commit: no need to let it fail silently
    or to wrap commands, hide output, etc etc... Just run the standard
    commands, that's it.

--- hydra-gtk/make_xhydra.sh
+++ hydra-gtk/make_xhydra.sh
@@ -1,20 +1,9 @@
-#!/bin/bash
-PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/gnome/lib/pkgconfig
-export PKG_CONFIG_PATH
-echo "Trying to compile xhydra now (hydra gtk gui) - don't worry if this fails, this is really optional ..."
-./configure > /dev/null 2> errors
-test -e Makefile || {
-  echo "Error: configure wasnt happy. Analyse this:"
-  cat errors
-  exit 1
-}
-make > /dev/null 2> errors
-test -e src/xhydra || {
-  echo "Error: could not compile. Analyse this:"
-  cat errors
-  echo
-  echo 'Do not worry, as I said, xhydra is really optional. ./hydra is ready to go!'
-  exit 0
-}
+#!/bin/sh
+
+set -e
+
+echo "Trying to compile xhydra now (hydra gtk gui)"
+./configure
+make
 cp -v src/xhydra ..
 echo "The GTK GUI is ready, type \"./xhydra\" to start"
